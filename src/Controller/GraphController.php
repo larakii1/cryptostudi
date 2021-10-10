42910f304e8dafed076fb47147bc6471a1be940c
larakii1 <abdelghafourelkanbi@gmail.com>
2021-10-10 22:52:14 +0200

    create Graph symfonyUx

--- src/Controller/GraphController.php
+++ src/Controller/GraphController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\PriceVariation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class GraphController extends AbstractController
 {
     #[Route('/graph', name: 'graph')]
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder, PriceVariation $priceVariation): Response
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([/* ... */]);
+
         return $this->render('graph/index.html.twig', [
-            'controller_name' => 'GraphController',
+            'chart' => $chart,
         ]);
     }
 }
